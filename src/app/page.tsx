3843a31df872927358d5153fb6b11a4e1c57231b
Executive Av <executiveavllc@Executives-MacBook-Pro.local>
2023-10-17 14:36:44 -0400

    updated desgin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,18 @@ export default function Home() {
       </header>
       <div className="buttons">
         <button onClick={() => window.location.href='https://www.instagram.com/exec.studios/'}>
-          <p>Connect: @Exec.Studios</p>
+          <p>Exec Studios | Insta</p>
+        </button>        
+        <button onClick={() => window.location.href='https://alejandro0684.wixsite.com/exec-studios'}>
+          <p>Exec Studios | Web</p>
         </button>
         <button onClick={() => window.location.href='https://www.executiveav.com/'}>
           <p>Executive Audio Visual</p>
         </button>
-        <button onClick={() => window.location.href='https://alejandro0684.wixsite.com/exec-studios'}>
-          <p>Executive Studios</p>
-        </button>
-        <button onClick={() => window.location.href='https://www.luxbyexecutive.com/'}>
-          <p>Lux by Executive</p>
-        </button>
       </div>
-    </div>
+      <footer>
+
+      </footer>
+  </div>
   )
 }
